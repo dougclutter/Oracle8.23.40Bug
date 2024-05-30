7fda33aa906539629696e82d9bbfa44be9720cf2
Doug Clutter <ClutterD@michigan.gov>
2024-05-30 08:41:26 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using BugCode.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+var configuration = new ConfigurationBuilder()
+    .AddUserSecrets(typeof(Program).Assembly)
+    .Build();
+
+var connectionString = configuration.GetConnectionString("MyConnection")
+    ?? "Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=10.123.123.123)(PORT=1521))(CONNECT_DATA=(UR=A)(SERVICE_NAME=myServiceName)));User Id=myId;Password=myPassword";
+
+var dbContextOptionsBuilder = new DbContextOptionsBuilder<MyDbContext>();
+dbContextOptionsBuilder.UseOracle(connectionString);
+var dbContext = new MyDbContext(dbContextOptionsBuilder.Options);
+
+var onlyWithDetails = true;
+var filterName = onlyWithDetails ? "with" : "without";
+
+/*
+ * This query works with Oracle.EntityFrameworkCore 8.21.121.
+ * This query fails with Oracle.EntityFrameworkCore 8.23.40; it returns an ORA-00904: "FALSE": invalid identifier.
+ */
+
+Console.WriteLine($"Finding all invoices {filterName} details:");
+var invoices = dbContext.Invoices
+    .Where(i => (i.InvoiceDetails.Count != 0) == onlyWithDetails)
+    .Select(i => $"Invoice ID: {i.Id} Name: {i.Name}");
+
+foreach (var invoice in invoices)
+{
+    Console.WriteLine(invoice);
+}
